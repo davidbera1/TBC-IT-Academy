92cae57e8d38e271091df3fa4b4eb549a87484a6
davidbera1 <dataaberadze@gmail.com>
2024-12-09 10:33:22 +0400

    Shemajamebeli 1

--- app/src/main/java/com/example/learnandroid/MainActivity.kt
+++ app/src/main/java/com/example/learnandroid/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.learnandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputList: MutableList<String>
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,7 +22,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        inputList = mutableListOf<String>()
+
+        binding.saveButton.setOnClickListener {
+            val input = binding.inputAnagram.text.toString()
+            if (input.isNotEmpty()) {
+                inputList.add(input)
+            }
+        }
+
+        binding.outputButton.setOnClickListener {
+            binding.tvResult.text = sortAnagrams(inputList).toString()
+        }
+
+        binding.clearButton.setOnClickListener {
+            inputList.clear()
+            binding.inputAnagram.text.clear()
+            binding.tvResult.text = ""
+        }
 
 
     }
+
+    private fun sortAnagrams(list: MutableList<String>): MutableList<List<String>> {
+
+        val groupedAnagrams = list.groupBy { it.toCharArray().sorted().joinToString("") }
+
+        val sortedAnagrams = groupedAnagrams.values.map { it.sorted() }
+
+        return sortedAnagrams.toMutableList()
+    }
+
 }
