182e91c8b324681f015415f7b8c97de752193f28
davidbera1 <dataaberadze@gmail.com>
2024-12-26 21:51:46 +0400

    homework 11

--- app/src/main/java/com/example/learnandroid/EditAddressFragment.kt
+++ app/src/main/java/com/example/learnandroid/EditAddressFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.learnandroid.databinding.FragmentAddNewAddressBinding
 import com.example.learnandroid.databinding.FragmentEditAddressBinding
 
 class EditAddressFragment : Fragment() {
