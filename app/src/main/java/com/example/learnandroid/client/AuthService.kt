91302ef7f6f0585acfb90db927ed279cd5b300f8
davidbera1 <dataaberadze@gmail.com>
2025-01-28 17:21:45 +0400

    homework 19 - finished, using Preferences DataStore

--- app/src/main/java/com/example/learnandroid/client/AuthService.kt
+++ app/src/main/java/com/example/learnandroid/client/AuthService.kt
@@ -3,10 +3,8 @@ package com.example.learnandroid.client
 import com.example.learnandroid.model.dto.AuthDto
 import com.example.learnandroid.model.dto.LoginResponseDto
 import com.example.learnandroid.model.dto.RegisterResponseDto
-import com.example.learnandroid.model.dto.UsersDto
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthService {
